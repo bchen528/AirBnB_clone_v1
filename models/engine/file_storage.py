fb741f5279d9acf7cd899444850db7eca1a17745
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-06-09 22:17:00 +0000

    format docstrings

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,26 +2,26 @@
 import json
 from os import path
 from models.base_model import BaseModel
-""" """
+"""File Storage Module"""
 
 
 class FileStorage:
-    """ """
+    """Class FileStorage Module"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ """
+        """Returns all the objects of each instance"""
         return self.__objects
 
     def new(self, obj):
-        """ """
+        """Creates a new obj"""
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
-        """ """
+        """Saves a dictionary to __file_path"""
         with open(self.__file_path, mode='w', encoding='utf-8') as f:
             new_dict = {}
             for key, value in self.__objects.items():
@@ -29,7 +29,7 @@ class FileStorage:
             f.write(json.dumps(new_dict))
 
     def reload(self):
-        """ """
+        """Loads the JSON from __file_path to self.__objects"""
         try:
             if path.exists(self.__file_path) and path.getsize("file.json") > 0:
                 with open(self.__file_path, mode='r', encoding='utf-8') as f:
