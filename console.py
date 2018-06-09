1a06625cfd97ec5e307f3bc70e372eec666133ee
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-06-09 22:13:01 +0000

    format docstring for console

--- console.py
+++ console.py
@@ -6,14 +6,18 @@ from models.engine.file_storage import FileStorage
 import json 
 from os import path
 from models import storage
+from shlex import split
 
 class HBNBCommand(cmd.Cmd):
     """Simple command processor for python"""
     prompt = '(hbtn) '
     all_classes = ["BaseModel", "FileStorage"]    
-
+    
     def do_create(self, line):
-        """ """
+        """Creates a new BaseModel, saves it to storage
+        Args:
+            line (string): string of all the commands
+        """
         if line:
             if line not in self.all_classes:
                 print("** class doesn't exist **")
@@ -25,9 +29,12 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def get_instance(self, line):
-        """Returns an instance value"""
+        """Returns an instance value
+        Args:
+            line (string): string of all the commands
+        """
         if line:
-            args = line.split()
+            args = split(line)
             if args[0] not in self.all_classes:
                 print("** class doesn't exist **")
             elif len(args) < 2:
@@ -35,23 +42,24 @@ class HBNBCommand(cmd.Cmd):
             else:
                 #search for id 
                 try:
-                    with open("file.json", mode='r', encoding='utf-8') as f:
-                        flag = 1
-                        for key, value in json.load(f).items():
-                            if args[1] == key.split('.')[1]:
-                                flag = 0
-                                return args[0], args[1], value
-                        if flag:
-                            print("** no instance found **")
+                    base = args[0] + '.' + args[1]
+                    for k, v in storage.all().items():
+                        if k == base:
+                            return v, args[1]
+                    else:
+                        print("** no instances found **")
                 except Exception:
                     print("** no instance found **")
         else:
             print("** class name missing **")
     
     def get_attribute(self, line):
-        """Gets the attribute name and value"""
+        """Gets the attribute name and value
+        Args:
+            line (string): string of all the commands
+        """
         if line:
-            args = line.split()
+            args = split(line)
             if len(args) < 3:
                 print("** attribute name missing **")
             elif len(args) < 4:
@@ -59,10 +67,15 @@ class HBNBCommand(cmd.Cmd):
             return args[2], args[3]
              
     def do_update(self, line):
-        """ """
+        """Updates your base models
+        Args:
+            line (string): string of all the commands
+        """
         try:
-            model, model_id, model_value = self.get_instance(line)
-            attr, attr_value = self.get_attribute(line)
+            model_value, model_id = self.get_instance(line)
+            attr, attr_value = self.get_attribute(line) 
+            setattr(model_value, attr, attr_value)
+            storage.save()
         except Exception:
             pass
 
@@ -70,15 +83,20 @@ class HBNBCommand(cmd.Cmd):
         """Prints the string representations of an instance based 
         on the class name and id
         ex: $show BaseModel 1234-1234-1234" # shows the entire dict
+        Args:
+            line (string): string of all the commands
         """
         try:
-            model, model_id, model_value = self.get_instance(line)
-            print("[{}] ({}) {}".format(model, model_id, model_value))
+            model_value, model_id = self.get_instance(line)
+            print("{}".format(model_value))
         except Exception:
             pass
 
     def do_all(self, line):
-        """Prints all the string representations of all instances in file.json"""
+        """Prints all the string representations of all instances in file.json
+        Args:
+            line (string): string of all the commands
+        """
         if line == "" or line in self.all_classes:
             try:
                 obj_list = []
@@ -90,9 +108,12 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
 
     def do_destroy(self, line):
-        """Deletes an instance based on teh class name and id"""
+        """Deletes an instance based on teh class name and id
+        Args:
+            line (string): string of all the commands
+        """
         if line:
-            args = line.split()
+            args = split(line)
             if args[0] not in self.all_classes:
                 print("** class doesn't exist **")
             elif len(args) != 2:
@@ -111,11 +132,20 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")         
 
     def do_quit(self, line):
-        """Quit command to exit the program"""
+        """Quit command to exit the program
+        Args:
+            line (string): string of all the commands
+        """
         return True
 
+    def emptyline(self):
+        pass
+
     def do_EOF(self, line):
-        """Exits the cmd"""
+        """Exits the cmd
+        Args:
+            line (string): string of all the commands
+        """
         return True
 
 if __name__ == '__main__':
