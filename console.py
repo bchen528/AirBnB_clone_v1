90b6e79052bdc0d0d95dcb1e468bdc64192d92ae
bchen528 <bchen720@gmail.com>
2018-06-10 18:27:01 +0000

    add show method

--- console.py
+++ console.py
@@ -4,15 +4,14 @@ import cmd
 from sys import argv
 from models.base_model import BaseModel
 from models import storage
+import json
 
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand"""
-
     def do_create(self, cls):
         """create a new instance of BaseModel
         """
-        print(cls)
         if cls == "":
             print("** class name missing **")
         elif cls != "BaseModel":
@@ -22,28 +21,48 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
             print(new.id)
 
-    def do_show(self, line):
+    def do_show(self, argv):
         """prints string representation of an instance based on class name, id
         """
 
+        args = "".join(argv)
+        args = [i.strip() for i in args.split(' ')]
+        if len(args) == 2:
+            cls_id = args[0] + "." + args[1]
+        flag = 0
+
         """
-        if cls == "":
+        print(args)
+        print("\n".join(args))
+        if len(args) == 2:
+            print(cls_id)
+        """
+        if len(args) == 0 or args[0] == "":
             print("** class name missing **")
-        elif cls != "BaseModel":
+        elif args[0] != "BaseModel":
             print("** class doesn't exist **")
-        elif obj_id == "":
+        elif len(args) == 1 or args[1] == "":
             print("** instance id missing **")
-        elif type(obj_id).__name__ != "BaseModel":
-            print("** no instance found **")
-        else:
-            print(obj_id.__str__())
-        """
+        elif len(args) == 2:
+            for key, value in (storage.all()).items():
+                if key == cls_id:
+                    print(value)
+                    flag = 1
+            if flag == 0:
+                print("** no instance found **")
+
     """
-    def do_all(self, cls):
-        if cls != "BaseModel":
+    def do_all(self, line):
+        if line != "BaseModel":
             print("** class doesn't exist **")
         else:
-            print( __str__())
+            print(line.__str__())
+    """
+
+    """
+    def parseline(self, line):
+        parse commandline
+        return cmd.Cmd.parseline(self, line)
     """
 
     def emptyline(self):
@@ -57,6 +76,15 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """Exit"""
         return True
+
+    """
+    def parse(argv):
+        args = []
+        for i in range(len(argv)):
+            if 
+            args.append = argv[1]
+        return args
+    """
 
 if __name__ == '__main__':
     display = HBNBCommand()
