188b23e35006aadf52e99a3bc068dae73f134255
bchen528 <bchen720@gmail.com>
2018-06-10 21:45:37 +0000

    add destroy method

--- console.py
+++ console.py
@@ -24,19 +24,8 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, argv):
         """prints string representation of an instance based on class name, id
         """
-
         args = "".join(argv)
         args = [i.strip() for i in args.split(' ')]
-        if len(args) == 2:
-            cls_id = args[0] + "." + args[1]
-        flag = 0
-
-        """
-        print(args)
-        print("\n".join(args))
-        if len(args) == 2:
-            print(cls_id)
-        """
 
         if len(args) == 0 or args[0] == "":
             print("** class name missing **")
@@ -45,19 +34,42 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) == 1 or args[1] == "":
             print("** instance id missing **")
         elif len(args) == 2:
-            for key, value in (storage.all()).items():
-                if key == cls_id:
-                    print(value)
-                    flag = 1
-            if flag == 0:
+            a_dict = storage.all()
+            key = args[0] + "." + args[1]
+            if key in a_dict:
+                print(a_dict[key])
+            else:
                 print("** no instance found **")
 
     def do_all(self, line):
+        """Prints all string representation of all instances based or not
+        on the class name
+        """
         if line != "BaseModel":
             print("** class doesn't exist **")
         else:
             for key, value in (storage.all()).items():
                 print([value])
+
+    def do_destroy(self, argv):
+        """deletes an instance based on the class name and id"""
+        args = "".join(argv)
+        args = [i.strip() for i in args.split(' ')]
+
+        if len(args) == 0 or args[0] == "":
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1 or args[1] == "":
+            print("** instance id missing **")
+        elif len(args) == 2:
+            a_dict = storage.all()
+            key = args[0] + "." + args[1]
+            if key in a_dict:
+                del a_dict[key]
+                storage.save()
+            else:
+                print("** no instance found **")
 
     def emptyline(self):
         pass
