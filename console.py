6a4e0409776415f3c0db5e69abcd4782a6309b9d
bchen528 <bchen720@gmail.com>
2018-06-10 23:50:00 +0000

    add update and edit all

--- console.py
+++ console.py
@@ -45,11 +45,11 @@ class HBNBCommand(cmd.Cmd):
         """Prints all string representation of all instances based or not
         on the class name
         """
-        if line != "BaseModel":
-            print("** class doesn't exist **")
-        else:
+        if line == "" or line == "BaseModel":
             for key, value in (storage.all()).items():
                 print([value])
+        else:
+            print("** class doesn't exist **")
 
     def do_destroy(self, argv):
         """deletes an instance based on the class name and id"""
@@ -70,6 +70,38 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
             else:
                 print("** no instance found **")
+
+    def do_update(self, argv):
+        """updates an instance based on the class name and id by
+        adding or updating attribute
+        """
+        args = "".join(argv)
+        args = [i.strip() for i in args.split(' ')]
+
+        if len(args) == 0 or args[0] == "":
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1 or args[1] == "":
+            print("** instance id missing **")
+        elif len(args) == 2:
+            a_dict = storage.all()
+            key = args[0] + "." + args[1]
+            if key not in a_dict:
+                print("** no instance found **")
+            else:
+                print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            a_dict = storage.all()
+            key = args[0] + "." + args[1]
+            if key in a_dict:
+                subkey = args[2]
+                subvalue = args[3].replace('"', '')
+                subdict = a_dict[key]
+                setattr(subdict, subkey, subvalue)
+                storage.save()
 
     def emptyline(self):
         pass
