f5095d5a75f9ec3baf2b1c1f26de15e7cf9127f4
bchen528 <bchen720@gmail.com>
2018-06-11 14:12:54 +0000

    first commit

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a class User"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """class User"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
