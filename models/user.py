91ad6f3d2c7dbaaea956296f2348bc61469fa503
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-06-12 15:34:44 +0000

    Additional Class unittests, model.user class addition

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/user/bin/evn python3
+"""User Module """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
